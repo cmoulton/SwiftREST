75fde8a5bc4a7285e379c2826cf9de75e5516a5b
Christina Moulton <christina@teakmobile.com>
2015-03-07 12:49:28 -0400

    Display JSON data in custom detail view

--- SwiftRest/SpeciesDetailViewController.swift
+++ SwiftRest/SpeciesDetailViewController.swift
@@ -41,6 +41,47 @@ class SpeciesDetailViewController: UIViewController {
         self.descriptionLabel!.text! += "Members of the \(name) species speak \(language)."
       }
       
+      if let height = self.species!.averageHeight
+      {
+        self.descriptionLabel!.text! += " The \(self.species!.name!) can be identified by their height, typically \(self.species!.averageHeight!)cm."
+      }
+      
+      var eyeColors:String?
+      if let colors = self.species!.eyeColors
+      {
+        eyeColors = ", ".join(colors)
+      }
+      var skinColors:String?
+      if let colors = self.species!.skinColors
+      {
+        skinColors = ", ".join(colors)
+      }
+      var hairColors:String?
+      if let colors = self.species!.hairColors
+      {
+        hairColors = ", ".join(colors)
+      }
+      
+      if eyeColors != nil && skinColors != nil && hairColors != nil
+      {
+        // if any of the colors, tack 'em on
+        self.descriptionLabel!.text! += "\n\nTypical coloring includes eyes:\n\t\(eyeColors!)\nhair:\n\t\(hairColors!)\nand skin:\n\t\(skinColors!)"
+      }
+    }
+
+    if self.species?.averageLifespan != nil
+    {
+      // some species have numeric lifespans (like 100) and some have lifespans like "indefinite", so we handle both by adding " years" to the numeric ones
+      self.descriptionLabel!.text! += "\n\nTheir average lifespan is \(self.species!.averageLifespan!)"
+      let years = self.species?.averageLifespan?.toInt()
+      if years != nil
+      {
+        self.descriptionLabel!.text! += " years."
+      }
+      else
+      {
+        self.descriptionLabel!.text! += "."
+      }
     }
     self.descriptionLabel!.sizeToFit() // to top-align text
   }
