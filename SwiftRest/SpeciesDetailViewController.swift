313ab12bbea303cf4405b60f8cf81c4e4e0c284f
Christina Moulton <christina@teakmobile.com>
2015-03-07 09:31:36 -0400

    Added SpeciesDetailViewController file

---
+++ SwiftRest/SpeciesDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SpeciesDetailViewController.swift
+//  SwiftRest
+//
+//  Created by Christina Moulton on 2015-03-01.
+//  Copyright (c) 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SpeciesDetailViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+}
