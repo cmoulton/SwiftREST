4fae1a762e8416987d5f16ba4ecc0a59554e0068
Christina Moulton <christina@teakmobile.com>
2015-03-08 15:20:39 -0400

    Add new search controller for getting images

---
+++ SwiftRest/DuckDuckGoSearchController.swift
@@ -0,0 +1,9 @@
+//
+//  DuckDuckGoSearchController.swift
+//  SwiftRest
+//
+//  Created by Christina Moulton on 2015-03-08.
+//  Copyright (c) 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
