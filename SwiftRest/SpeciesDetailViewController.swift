80c692493238e9cc6b9bca60f538b4821bd84112
Christina Moulton <christina@teakmobile.com>
2016-12-06 15:25:20 -0500

    Added new detail view (no data displaying yet but it’s all hooked up in the storyboard)

---
+++ SwiftRest/SpeciesDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SpeciesDetailViewController.swift
+//  SwiftRest
+//
+//  Created by Christina Moulton on 2016-12-06.
+//  Copyright © 2016 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SpeciesDetailViewController: UIViewController {
+  @IBOutlet var descriptionLabel: UILabel?
+  var species: StarWarsSpecies?
+}
